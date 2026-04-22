7c9dd4b7adc9708c34a8d19fc0d86133d8154ce8
chamadiaaarav-maker <chamadia.aarav@gmail.com>
2026-04-22 21:06:25 +0400

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,4 @@
+import keyword
+
+print("The keywords for python are...\n")
+print(keyword.kwlist)
---
+++ main.py
@@ -0,0 +1,6 @@
+a = input("Enter a value:")
+b = input("Enter a value:")
+c = input("Enter a value:")
+
+a, b, c = b, c, a
+print(f"a={a}, b={b}, c={c}")
