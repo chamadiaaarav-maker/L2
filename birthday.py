cf171792b0d1c7f6bd1aacb16688ba9d8362f61d
chamadiaaarav-maker <chamadia.aarav@gmail.com>
2026-04-22 21:31:22 +0400

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,7 @@
+alex = "21 JUN 2013"
+sam = "1 OCT 2016"
+george = "13 SEP 2017"
+may = "1 NOV 2017"
+paula = "1 DEC 2016"
+
+print(f"Birthay's of Raj's friends:\n Alex = {alex}, Sam = {sam}, George = {george}, May = {may}, Paula = {paula}")
